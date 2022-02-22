12135bc315e5a1b279325837ee4d7ea4c0ede711
Chetandada28 <chetanhole@hotmail.com>
2022-02-22 20:23:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-
+const INDEX = '/synergy009.html';
 
 const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
