109d6e59eebaa3d242ac4ba1a9ab5f23e30ee1aa
Chetandada28 <chetanhole@hotmail.com>
2022-02-22 20:30:51 +0530

    Update serc.js

--- serc.js
+++ serc.js
@@ -1,31 +1,8 @@
-const WebSocket = require('ws');
+ let HOST = location.origin.replace(/^http/, 'ws')
+      let ws = new WebSocket(HOST);
+      let el;
 
-     
-   
-
-const wss =  new WebSocket(HOST);
-
-
-
-wss.on('connection', function connection(wss) {
-  wss.on('message', function incoming(message) {
-
-    //Make incoming JSON into javascript object
-    var msg = JSON.parse(message)
-
-    // Print whole message to console
-    console.log(msg)
-
-    // Print only message type to console. For example BootNotification, Heartbeat etc...
-   console.log("Message type: " + msg[2])
-
-    // Send response depending on what the message type is
-    if (msg[2] === "BootNotification") {
-      //Send correct response
-
-    } // Add all the message types
-
-  });
-
-
-});
+      ws.onmessage = (event) => {
+        el = document.getElementById('server-response');
+        el.innerHTML = 'server-response: ' + event.data;
+      };
